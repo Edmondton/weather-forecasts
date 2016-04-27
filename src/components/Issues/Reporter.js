eb94a96d0f755b295b3bf543f6730fe2072a4d9c
edmondton <edmondton@gmail.com>
2016-04-27 17:13:27 -0400

    init

--- src/components/Issues/Reporter.js
+++ src/components/Issues/Reporter.js
@@ -3,14 +3,20 @@ import Title from './Title';
 import styles from './Issues.scss';
 
 export default function Reporter ({ issue }) {
+    const url = issue.getIn(['user', 'html_url']);
+
     return (
         <div>
-            <img
-                src={issue.getIn(['user', 'avatar_url'])}
-                className={styles.avatar}
-            />
+            <a href={url} className={styles.avatarLink} >
+              <img
+                  src={issue.getIn(['user', 'avatar_url'])}
+                  className={styles.avatar}
+              />
+            </a>
             <p className={styles.userLogin}>
+              <a href={url}>
                 @{issue.getIn(['user', 'login'])}
+              </a>
             </p>
         </div>
     );
