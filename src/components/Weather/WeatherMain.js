974f465914c835dcca804004f0f6bbc3a5b65347
Edmond Chow <Edmondton@gmail.com>
2016-05-02 22:40:06 -0400

    init

--- src/components/Weather/WeatherMain.js
+++ src/components/Weather/WeatherMain.js
@@ -11,48 +11,56 @@ import styles from './styles.scss';
 class WeatherMain extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            units: 'imperial'
+        }
     }
 
     componentDidMount() {
+        const {units} = this.state;
         const {city} = this.props;
         if (city) {
-            this.getWeather({
-                params: {
-                    q: city,
-                    units: 'metric'
-                }
-            });
+            this.getWeather(city, units);
         }
     }
 
     onChange(event) {
+        const {units} = this.state;
         const city = event.target.value;
-        this.getWeather({
-            params: {
-                q: city,
-                units: 'metric'
-            }
-        });
+        this.getWeather(city, units);
     }
 
-    getWeather(params) {
+    getWeather(city, units) {
         const {dispatch} = this.props;
-        dispatch(fetchWeather(params));
+        dispatch(fetchWeather({
+            params: {
+                q: city,
+                units: units
+            }
+        }));
     }
 
     getDays(temps) {
         if (temps) {
             return temps.map((item, key) => {
                 return (
-                    <WeatherDay date={key} data={item} key={key} />
+                    <WeatherDay date={key} data={item} key={key} isMetric={this.state.units === 'metric'} />
                 );
             });
         }
         return null;
     }
 
+    onUnitChange(units) {
+        this.setState({
+            'units': units
+        });
+        const {city} = this.props;
+        this.getWeather(city, units);
+    }
+
     render() {
-        const {weather} = this.props;
+        const {weather, units} = this.props;
         const days = this.getDays(weather.get('temps'));
         return (
             <div className={styles.container}>
@@ -61,7 +69,18 @@ class WeatherMain extends Component {
                                  onBlur={this.onChange.bind(this)}
                                  defaultValue={this.props.city}
                                  className={styles.cityInput}
-                />
+                            />
+                    <span className={styles.unitSelection}>
+                        <input type="radio"
+                                name="group"
+                                id="far"
+                                defaultChecked
+                                onChange={this.onUnitChange.bind(this, 'imperial')}
+                        />
+                        <label htmlFor="far">Fahrenheit</label>
+                        <input type="radio" name="group" id="cel" onChange={this.onUnitChange.bind(this, 'metric')} />
+                        <label htmlFor="cel">Celsius</label>
+                    </span>
                 </div>
                 <header className={styles.header}>
                     Weather Forecast For {weather.getIn(['city', 'name'])}
